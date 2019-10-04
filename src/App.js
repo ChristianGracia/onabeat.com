96a00c1d2c5f10698f3925f3c320c68acf30c6e9
Christian Gracia <christiangracia.student@careerdevs.com>
2019-10-04 02:30:23 -0400

    added dependencies and styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <h1>On A Beat</h1>
+      <h1 style={{ marginTop: 100 }}>On A Beat</h1>
       <h2>Under construction</h2>
+      <a href="https://www.youtube.com/channel/UC2bzDGJGflVc9i4Yfx019Hg">Youtube</a>
       <span>coming soon</span>
       <p>Redesign on the way by <a href="http://christiangracia.com">christiangracia.com</a></p>
     </div>
