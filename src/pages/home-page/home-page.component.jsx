408a27894a4b94df375cffe692b478653c44c0be
Christian Gracia <christiangracia.student@careerdevs.com>
2019-10-04 16:29:19 -0400

    added icons and soundcloud link

--- src/pages/home-page/home-page.component.jsx
+++ src/pages/home-page/home-page.component.jsx
@@ -8,7 +8,10 @@ const HomePage = () => (
         <p style={{ marginTop: 20, fontWeight: "bold", color: "white", fontSize: 20 }}>Videos, Music, & More</p>
         <h2 style={{ fontWeight: "bold", color: "yellow" }}>Under construction</h2>
         <div style={{ padding: 10 }}></div>
-        <a href="https://www.youtube.com/channel/UC2bzDGJGflVc9i4Yfx019Hg">Youtube</a>
+        <p style={{ color: "white" }}>Contact <i class="fas fa-envelope"></i> xp@onabeat.com am@onabeat.com</p>
+        <span>
+            <a href="https://www.youtube.com/channel/UC2bzDGJGflVc9i4Yfx019Hg">Youtube</a>{"\t"}
+            <a href="https://soundcloud.com/amonabeat">Soundcloud</a></span>
         <p style={{ marginTop: 20, color: "white", fontSize: 20 }}>Redesign on the way by <a href="http://christiangracia.com">christiangracia.com</a></p>
         <p style={{ fontWeight: "bold", fontSize: 30, color: "white" }}>coming soon</p>
         <i style={{ fontSize: 40, color: "#0EFF00" }} className="fas fa-spinner fa-spin"></i>
