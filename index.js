c20e6e94eb490741c9bb1d5b45591ae165448d5e
Christian Gracia <christiangracia.student@careerdevs.com>
2019-12-12 00:03:25 -0500

    deploy

--- index.js
+++ index.js
@@ -9,6 +9,22 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+const publicPath = path.join(__dirname, "/client", "public");
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log("Server is up!");
+});
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "client/build")));
+
+  app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "client/build", "index.html"));
+  });
+}
+
 const database = require("./config/keys").mongoURI;
 
 const options = {
@@ -21,34 +37,7 @@ mongoose
   .then(() => console.log("connected to MongoDB"))
   .catch(err => console.log("db connection error"));
 
-if (process.env.NODE_ENV === "production") {
-  // Set static folder
-  app.use(express.static("client/build"));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
-
-// app.use(express.static(path.join(__dirname, "./client/public")));
-
-// app.get("*", function(_, res) {
-//   res.sendFile(path.join(__dirname, "./client/public/index.html"), function(
-//     err
-//   ) {
-//     if (err) {
-//       res.status(500).send(err);
-//     }
-//   });
-// });
-
-app.get("/", (req, res) => res.send("server is up"));
-
 app.get("/all", cors(), (req, res) => {
   res.json({ hi: "cors works" });
   console.log("hit");
 });
-
-const port = process.env.PORT || 5000;
-
-app.listen(port, () => console.log(`listening on port ${port}`));
