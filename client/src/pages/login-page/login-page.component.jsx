4752adf46f80976e866a2b8302b0c825e57e4b2c
Christian Gracia <christiangracia.student@careerdevs.com>
2019-12-18 22:36:54 -0500

    added info and animations to login page

--- client/src/pages/login-page/login-page.component.jsx
+++ client/src/pages/login-page/login-page.component.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import logo from "../../assets/logo.PNG";
 import "./login-page.styles.scss";
 import LoginForm from "../../components/login-form/login-form.component";
+import LightSpeed from "react-reveal/LightSpeed";
+import Tada from "react-reveal/Tada";
 
 const LoginPage = () => (
   <div style={{ justifyContent: "center", alignItems: "center" }}>
@@ -16,8 +18,22 @@ const LoginPage = () => (
         }}
       />
     </div>
-    <p style={{ color: "red", fontWeight: "bold", fontSize: 30 }}>Login</p>
+    <p style={{ color: "red", fontWeight: "bold", fontSize: 30 }}>
+      Site Dashboard Login
+    </p>
     <LoginForm />
+    <LightSpeed left>
+      <p style={{ marginTop: 20, color: "red", fontSize: 14 }}>
+        Only <span style={{ fontWeight: "bold", color: "red" }}>On a Beat</span>{" "}
+        members have the ability create accounts
+      </p>
+    </LightSpeed>
+    <Tada>
+      {" "}
+      <p style={{ fontWeight: "bold", color: "red", fontSize: 25 }}>
+        For now...
+      </p>
+    </Tada>
   </div>
 );
 export default LoginPage;
