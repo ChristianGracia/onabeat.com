3aadea7936c4c6b2c459f50fa20d16a7671541ed
Christian Gracia <christiangracia.student@careerdevs.com>
2019-12-18 22:06:47 -0500

    added styling to about page

--- client/src/pages/about-page/about-page.component.jsx
+++ client/src/pages/about-page/about-page.component.jsx
@@ -19,28 +19,44 @@ const AboutPage = () => (
 
     <div
       style={{
-        minWidth: 320,
-        maxWidth: "50%",
         display: "flex",
-        flexDirection: "column"
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        padding: 30,
+        color: "red"
       }}
     >
-      <p>
-        On A Beat is a performance company. We create shows for all occasions
-        (parties dinners dances religious services corporate functions &c) and
-        locations (clubs, bars, weddings, galleries, churches corners.){" "}
-      </p>{" "}
-      <p>
-        Our ultimate goal is to create pan American symphonies , musicals , and
-        operas of unprecedented scale and quality. As the goal presupposes a
-        deep familiarity with music , musicians , and music lovers , we will
-        play almost anything almost any where.{" "}
-      </p>{" "}
-      <p>
-        Both more and less than a band: we feature a revolving group of artists,
-        and can be as big or small or the situation requires. Check our artist’s
-        page for past and current collaborators.
-      </p>
+      <div style={{}}>
+        <p style={{ fontSize: 27 }}>
+          <span style={{ fontWeight: "bold" }}>On A Beat</span> is a performance
+          company.{" "}
+        </p>
+      </div>
+      <div>
+        <p style={{ marginTop: 20 }}>
+          We create shows for all occasions (parties dinners dances religious
+          services corporate functions &c) and locations (clubs, bars, weddings,
+          galleries, churches corners.){" "}
+        </p>{" "}
+      </div>
+
+      <div style={{ marginTop: 20 }}>
+        <p>
+          Our ultimate goal is to create pan American symphonies, musicals, and
+          operas of unprecedented scale and quality. As the goal presupposes a
+          deep familiarity with music, musicians, and music lovers, we will play
+          almost anything almost any where.{" "}
+        </p>
+      </div>
+      {""}
+      <div style={{ marginTop: 20 }}>
+        <p>
+          Both more and less than a band: we feature a revolving group of
+          artists, and can be as big or small or the situation requires. Check
+          our artist’s page for past and current collaborators.
+        </p>
+      </div>
     </div>
   </div>
 );
