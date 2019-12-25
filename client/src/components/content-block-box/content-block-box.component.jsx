4900873bf5547b10f705b2dca2c622ce40381cfd
Christian Gracia <christiangracia.student@careerdevs.com>
2019-12-25 18:58:53 -0500

    added boostrap grid for content block organization (music, video, and a gif each block

--- client/src/components/content-block-box/content-block-box.component.jsx
+++ client/src/components/content-block-box/content-block-box.component.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+import SpotifyPlayer from "../spotify-player/spotify-player.component";
+import VideoPlayer from "../video-player/video-player.component";
+import { Container, Row, Col } from 'react-bootstrap';
 
 class ContentBlockBox extends React.Component {
     constructor(props) {
@@ -7,7 +10,23 @@ class ContentBlockBox extends React.Component {
 
     render() {
         return (
-            <div>Content</div>
+            <div>
+                <div>Content</div>
+                <Container >
+                    <Row>
+                        <Col>
+                            <VideoPlayer videoUrl="ZZ5LpwO-An4" />
+                        </Col>
+                        <Col>
+                            <Row><SpotifyPlayer songUrl="https://open.spotify.com/embed/track/59QmjU5K8XQTfj9JjFNb4Z" /></Row>
+                            <Row><span>Hi</span></Row>
+
+                        </Col>
+                    </Row>
+                </Container >
+
+
+            </div>
         )
     }
 }
