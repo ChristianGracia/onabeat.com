c5903d91105f822359241732f74a47dab9dd6039
Christian Gracia <christiangracia.student@careerdevs.com>
2019-12-26 13:09:44 -0500

    added scss sheet

--- client/src/components/content-block-box/content-block-box.component.jsx
+++ client/src/components/content-block-box/content-block-box.component.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import SpotifyPlayer from "../spotify-player/spotify-player.component";
 import VideoPlayer from "../video-player/video-player.component";
 import { Container, Row, Col } from 'react-bootstrap';
+import "./content-block-box.styles.scss";
+
+
 
 class ContentBlockBox extends React.Component {
     constructor(props) {
@@ -12,17 +15,17 @@ class ContentBlockBox extends React.Component {
         return (
             <div>
                 <div>Content</div>
-                <Container >
-                    <Row>
+                <div style={{ display: "flex", flex: 0, flexDirection: "row", justifyContent: "center", flexWrap: "wrap" }}>
 
+                    <div >
                         <VideoPlayer videoUrl="ZZ5LpwO-An4" />
+                    </div>
+                    {/* <iframe src="https://open.spotify.com/embed/track/59QmjU5K8XQTfj9JjFNb4Z" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe> */}
+                    <div > <SpotifyPlayer songUrl="https://open.spotify.com/embed/track/59QmjU5K8XQTfj9JjFNb4Z" /></div>
+                    <div style={{ maxWidth: "25%" }}><img src="https://img.webmd.com/dtmcms/live/webmd/consumer_assets/site_images/article_thumbnails/reference_guide/cats_and_excessive_meowing_ref_guide/1800x1200_cats_and_excessive_meowing_ref_guide.jpg" /></div><div >
+                        <iframe src="https://giphy.com/embed/11JTxkrmq4bGE0" frameBorder="0" class="" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/cat-computer-working-11JTxkrmq4bGE0"></a></p></div>
 
-                        {/* <iframe src="https://open.spotify.com/embed/track/59QmjU5K8XQTfj9JjFNb4Z" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe> */}
-                        <SpotifyPlayer songUrl="https://open.spotify.com/embed/track/59QmjU5K8XQTfj9JjFNb4Z" />
-                        <iframe src="https://giphy.com/embed/11JTxkrmq4bGE0" width="200" height="200" frameBorder="0" class="" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/cat-computer-working-11JTxkrmq4bGE0"></a></p>
-                    </Row>
-                </Container >
-
+                </div>
 
             </div >
         )
