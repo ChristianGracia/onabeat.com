376fbf91b736a5f21c0801405e68a7ed15128b3c
Christian Gracia <christiangracia.student@careerdevs.com>
2019-12-29 21:24:04 -0500

    add and delete block route set up

--- server/api/blocks.js
+++ server/api/blocks.js
@@ -7,27 +7,28 @@ const Block = require("../models/Block");
 router.get("/all", (req, res) => {
     Block.find()
         // .sort({ date: -1 })
-        .then(blocks => res.json(gifs))
-        .catch(err => res.status(404).json({ nogifsfound: "No gifs found" }));
+        .then(blocks => res.json(blocks))
+        .catch(err => res.status(404).json({ noblocksfound: "No blocks found" }));
 });
 
 router.post(
-    "/create-gif", (req, res) => {
-        const newGif = new Gif({
-            name: req.body.name,
-            gifUrl: req.body.gifUrl,
-            description: req.body.description
+    "/create-block", (req, res) => {
+        const newBlock = new Gif({
+            title: req.body.title,
+            vid: req.body.vid,
+            song: req.body.song,
+            pic: req.body.pic,
+            gif: req.body.gif
         });
-
-        newGif.save().then(gifs => res.json(gifs));
+        newBlock.save().then(blocks => res.json(blocks));
     }
 );
 
 router.delete("/:id", (req, res) => {
     console.log(req.body);
-    Gif.findById(req.params.id)
-        .then(gif => gif.remove().then(res.json({ success: "true" })))
-        .catch(err => res.status(404).json({ gifnotfound: "No gif found" }));
+    Block.findById(req.params.id)
+        .then(block => block.remove().then(res.json({ success: "true" })))
+        .catch(err => res.status(404).json({ blocknotfound: "No block found" }));
     res.send;
 });
 module.exports = router;
