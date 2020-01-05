a155997e76c14a3ce8fa9eca8ff5fc9cfeef3f58
Christian Gracia <christiangracia.student@careerdevs.com>
2020-01-04 21:30:44 -0500

    added soundcloud links and more styling

--- client/src/pages/artists-page/artists-page.component.jsx
+++ client/src/pages/artists-page/artists-page.component.jsx
@@ -18,7 +18,26 @@ const ArtistsPage = () => (
     <div style={{ display: "flex", flexDirection: "column" }}>
       <p style={{ color: "black", fontWeight: "bold", fontSize: 30 }}>Artists</p>
 
-      <span style={{ color: "#000", fontSize: 25, marginTop: 20, fontWeight: "bold" }}><span style={{ color: "#F8D208" }}>Agostino</span> - am@onabeat.com</span>
+      <span style={{ color: "#000", fontSize: 25, marginTop: 20, fontWeight: "bold" }}><span style={{ color: "#F8D208" }}>Agostino</span> </span>
+      <div style={{ textAlign: "center", marginTop: 20 }} className="social-links">
+        <a href="https://soundcloud.com/amonabeat">
+          <span style={{ paddingRight: 5, color: "black" }}>SoundCloud</span>
+          <span>
+            <i
+              className="fab fa-soundcloud"
+              style={{ color: "#FF5500", fontSize: 20 }}
+            ></i>
+          </span>
+        </a>
+        <span style={{ padding: 10 }}></span>
+
+        <span>
+          <i
+            className="fas fa-envelope"
+            style={{ color: "black", fontSize: 20 }}
+          ></i> am@onabeat.com
+          </span>
+      </div>
     </div>
   </div>
 );
