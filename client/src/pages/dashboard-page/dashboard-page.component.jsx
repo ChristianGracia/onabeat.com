0fee8e42d54b73149a97bc6d8591b7970b1d34e6
Christian Gracia <christiangracia.student@careerdevs.com>
2020-02-13 21:26:28 -0500

    removed unneeded code

--- client/src/pages/dashboard-page/dashboard-page.component.jsx
+++ client/src/pages/dashboard-page/dashboard-page.component.jsx
@@ -5,19 +5,7 @@ import EditSite from "../../components/edit-site/edit-site.component";
 
 const DashboardPage = () => (
   <div style={{ justifyContent: "center", alignItems: "center" }}>
-    {/* <div className="logo mr-auto ml-auto">
-      <div
-        className="logo-image"
-        style={{
-          backgroundImage: `url(
-            ${logo}
-          )`,
-          backgroundRepeat: "no-repeat"
-        }}
-      />
-    </div> */}
     <p style={{ color: "red", fontWeight: "bold", fontSize: 30 }}>Dashboard</p>
-
     <EditSite />
   </div>
 );
