5f5fc5581e8ec606eee84e56c507f7270633dfb4
Christian Gracia <christiangracia.student@careerdevs.com>
2020-03-08 14:39:21 -0400

    formatted schema for pic storage

--- server/models/Pic.js
+++ server/models/Pic.js
@@ -2,16 +2,16 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const PicSchema = new Schema({
-    name: {
-        type: String,
-        required: true
-    },
-    description: {
-        type: String
-    },
-    picUrl: {
-        type: String
-    }
+  name: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String
+  },
+  picUrl: {
+    type: String
+  }
 });
 
 module.exports = mongoose.model("pic", PicSchema);
