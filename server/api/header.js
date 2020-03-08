adddc9a3aa5435e9ed497e988a71c05b7786f7fc
Christian Gracia <christiangracia.student@careerdevs.com>
2020-03-08 14:52:57 -0400

    added put route to change header

--- server/api/header.js
+++ server/api/header.js
@@ -12,8 +12,23 @@ router.post("/create-header", (req, res) => {
   const newHeader = new Header({
     headerString: req.body.headerString
   });
-
   newHeader.save().then(header => res.json(header));
+});
+router.post("/change-header", (req, res) => {
+  Header.findOneAndUpdate("5e653cbaa853fb002defaf59", req.body.header, function(
+    err,
+    result
+  ) {
+    if (err) {
+      console.log(err);
+    }
+    console.log("RESULT: " + result);
+    res.send("Done");
+  });
+  // const newHeader = new Header({
+  //   headerString: req.body.headerString
+  // });
+  // newHeader.save().then(header => res.json(header));
 });
 
 module.exports = router;
